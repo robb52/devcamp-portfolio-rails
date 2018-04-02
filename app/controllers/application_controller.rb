574775032c626bd6db3cb480ec053c68f1137ff0
Rob Brown <brown4homes@yahoo.com>
2018-04-02 16:16:37 -0700

    Modified app HTML controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = BrownViewTool::Renderer.copyright "Rob Brown", "All rights reserved"
+  end
 end
+
