3808ec32bd54dff801af8e790b6e6c3f2dfc5c53
Rob Brown <brown4homes@yahoo.com>
2018-05-11 12:38:33 -0700

    Built out topic views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Admin User",
+  roles: "site_admin"
+  )
+  
+  puts "1 Admin user created"
+  
+User.create!(
+  email: "tes2@test.com",
+  password: "asdfasd",
+  password_confirmation: "asdfasd",
+  name: "Regular User",
+  )
+ 
+  puts "1 regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
