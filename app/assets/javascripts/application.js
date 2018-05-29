bc3eb08d762293d351a1d395342cfe4dd15f984a
Rob Brown <brown4homes@yahoo.com>
2018-05-29 15:17:23 -0700

    Fixed JS loading bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require bootstrap-sprockets
 //= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
 //= require turbolinks
 //= require_tree .
-
+//= require bootstrap-sprockets
